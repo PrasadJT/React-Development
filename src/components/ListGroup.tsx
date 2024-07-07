0b2af3092b5690fdb8717c85994ef3a2ba75acd7
Thilina Prasad <thilina1091@gmail.com>
2024-07-07 23:38:22 +0530

    Update ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,32 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  if (items.length === 0)
+    return (
+      <>
+        <></>
+        <h1>List</h1>
+        <p>No item found</p>;
+      </>
+    );
+
+  //const message = items.length === 0 ? <p>No item found</p> : null
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
